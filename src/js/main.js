0fc615cc88076e09f8de5ca3a1d32847893d821f
Ira <ira.dymova.1207@gmail.com>
2023-08-07 12:12:52 +0300

    start

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import './ui-elements/input-action'
+import './ui-elements/input-mask'
+import './ui-elements/button'
+import './ui-elements/select'
+import './ui-elements/ckeckbox-rules'
+import './form'
+import './popup'
+
+
