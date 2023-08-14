eb855e43d0960f684deff4bc976aad723b209b07
Ira <ira.dymova.1207@gmail.com>
2023-08-14 23:37:49 +0300

    main page - part 1

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -1,5 +1,61 @@
 D.addEventListener("DOMContentLoaded", function () {
 
+	//banner slider 
+	const bannerSlider = D.querySelectorAll('.js-banner');
+	bannerSlider.forEach(el => {
+		const swiper = new Swiper(el, {
+			slidesPerView: 1,
+			spaceBetween: 10,
+			speed: 600,
+			autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+			effect: "creative",
+      creativeEffect: {
+        prev: {
+          shadow: false,
+          translate: ["-20%", 0, -1],
+        },
+        next: {
+          translate: ["100%", 0, 0],
+        },
+      },
+			pagination: {
+				el: '.js-banner__pagination',
+				clickable: true,
+			}
+		});
+	})
+
+	
+//banner slider 
+	const bannerSl = D.querySelectorAll('.js-banner-sl');
+	bannerSl.forEach(el => {
+		const swiper = new Swiper(el, {
+			slidesPerView: 1,
+			spaceBetween: 0,
+			effect: "fade",
+			speed: 700,
+			effect: "creative",
+      creativeEffect: {
+        prev: {
+          shadow: false,
+          translate: ["-80%", 0, -500],
+        },
+        next: {
+          shadow: false,
+          translate: ["80%", 0, -500],
+        },
+      },
+			navigation: {
+				nextEl: ".js-banner-sl__next",
+				prevEl: ".js-banner-sl__prev",
+			},
+		});
+	})
+
+
 	//main slider 
 	const slider = D.querySelectorAll('.js-slider');
 	slider.forEach(el => {
@@ -68,4 +124,41 @@ D.addEventListener("DOMContentLoaded", function () {
 		}
 	})
 
+		//banner slider 
+		const videoSlider = D.querySelectorAll('.js-video-slider');
+		videoSlider.forEach(el => {
+			const swiper = new Swiper(el, {
+				slidesPerView: 1,
+				spaceBetween: 10,
+				speed: 600,
+				navigation: {
+					nextEl: ".js-video-slider__next",
+					prevEl: ".js-video-slider__prev",
+				},
+			});
+		})
+
+//news slider
+	const newsSlider = D.querySelectorAll('.js-news-slider');
+	newsSlider.forEach(el => {
+		const swiper = new Swiper(el, {
+			slidesPerView: 1,
+			spaceBetween: 15,
+			speed: 900,
+			navigation: {
+				nextEl: ".js-news-slider__next",
+				prevEl: ".js-news-slider__prev",
+			},
+			breakpoints: {
+				650: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				1000: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				}
+			}
+		});
+	})
 });
