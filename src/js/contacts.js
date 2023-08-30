0e11609fd6284525892c884fb898a61877559021
Andrey Zhelezko <because.ahaha@gmail.com>
2023-08-30 14:42:23 +0300

    fixes

--- src/js/contacts.js
+++ src/js/contacts.js
@@ -1,29 +1,32 @@
 const contactsMap = () => {
 
+    const pin = {
+        iconLayout: 'default#image',
+        iconImageHref: './../images/sprites/svg/pin.svg',
+        iconImageSize: [38, 38],
+        iconImageOffset: [-19,-19]
+    }
+
     let myMap = new ymaps.Map("map", {
         center: [55.852061, 37.530499],
-        zoom: 15
+        zoom: 16
     });
 
     myMap.geoObjects
         .add(new ymaps.Placemark([55.852061, 37.530499], {
-            balloonContent: 'г. Москва, 4-й Лихачёвский переулок, 4с2'
-        }, {
-            preset: 'islands#icon',
-            iconColor: '#FF671F'
-        }))
+            balloonContentBody: '<b>4-й Лихачёвский переулок, 4с2</b><br/> ' +
+                                '<a href="tel: +7-495-120-81-37">+7 (495) 120-81-37</a>',
+            balloonContentFooter: 'Офис: 08:00-18:00, касса 08:00-17:00'
+        }, pin))
         .add(new ymaps.Placemark([59.847037, 30.446627], {
-            balloonContent: 'г. СПб'
-        }, {
-            preset: 'islands#icon',
-            iconColor: '#FF671F'
-        }))
+            balloonContentBody: '<b>просп. Девятого Января, 9, корп. 1</b><br/> ' +
+                                '<a href="tel: +7-812-603-49-93">+7 (812) 603-49-93</a>',
+            balloonContentFooter: 'Офис: 08:00-18:00, касса 08:00-17:00'
+        }, pin))
         .add(new ymaps.Placemark([56.908266, 60.619106], {
-            balloonContent: 'г. ЕКБ'
-        }, {
-            preset: 'islands#icon',
-            iconColor: '#FF671F'
-    }));
+            balloonContentBody: '<b>жилой район Эльмаш</b>',
+            // balloonContentFooter: 'Офис: 08:00-18:00, касса 08:00-17:00'
+        }, pin));
 
 	const tabs = D.getElementById("js-tabs-map");
 	if (!tabs) return;
